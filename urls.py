33e86404a4ec5ec4eb0eb730dd4811cc506082a1
PythonXCII <dherkt92@gmail.com>
2020-01-27 20:47:14 +0100

    All files for Heroku 11

--- urls.py
+++ urls.py
@@ -3,7 +3,10 @@ from django.urls import include, path
 from fantasy_realms.main import urls
 from rest_framework.authtoken import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include(urls)),
     path('api-token-auth/', views.obtain_auth_token)]
+
+print('s')
