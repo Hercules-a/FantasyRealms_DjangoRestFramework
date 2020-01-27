1c8e5e9b1fb8009ef443239638caf7651f28e109
PythonXCII <dherkt92@gmail.com>
2020-01-27 21:23:06 +0100

    All files for Heroku 14

--- fantasy_realms/settings.py
+++ fantasy_realms/settings.py
@@ -80,7 +80,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'fantasy_realms.wsgi.application'
 
 
-default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, '../../db.sqlite3')
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
